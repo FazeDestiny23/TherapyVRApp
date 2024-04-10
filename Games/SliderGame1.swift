0d0c0b4f7990ded7cfe40a08ed05622fe28c331b
FazeDestiny23 <masterkibo2304@gmail.com>
2024-04-10 13:09:11 -0600

    New Game.

---
+++ Games/SliderGame1.swift
@@ -0,0 +1,18 @@
+//
+//  SliderGame1.swift
+//  UsuarioDatos
+//
+//  Created by FRANCISCO AQUINO on 10/04/24.
+//
+
+import SwiftUI
+
+struct SliderGame1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SliderGame1()
+}
